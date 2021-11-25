f19443c5f8f89872be4960106fd06dcaf1eef2c6
Darya Varlamova <d.varlamova@icloud.com>
2021-11-25 22:53:45 +0300

    commit1

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int sum_transfer = 500_000_000;
+        int total_balance = balance + sum_transfer;
+        System.out.println(total_balance);
+    }
+}
